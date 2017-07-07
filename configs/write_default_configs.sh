0af71462aa2b1fd5fe86e1f93393a465dc059193
Francesco Asnicar <francesco.asnicar@gmail.com>
2017-07-07 02:22:49 +0200

    resolving polytomies also for concatenation pipeline, write_config updated

--- configs/write_default_configs.sh
+++ configs/write_default_configs.sh
@@ -2,6 +2,6 @@
 
 
 # supermatrix.cfg
-python3 ../write_config_file.py -o supermatrix.cfg --map_dna --map_aa --trim --tree2
+python3 ../write_config_file.py -o supermatrix.cfg --map_dna --map_aa --trim --tree2 --overwrite
 # supertree.cfg
-python3 ../write_config_file.py -o supertree.cfg --map_dna --map_aa --trim --gene_tree1 --gene_tree2
+python3 ../write_config_file.py -o supertree.cfg --map_dna --map_aa --trim --gene_tree1 --gene_tree2 --overwrite
