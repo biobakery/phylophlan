3a2f90d13405a8f9dc092002f175459b5fba47d9
Francesco Asnicar <5664022+fasnicar@users.noreply.github.com>
2017-08-28 12:35:20 +0200

    bug fixes, added nucleotide database support, new tools in configuration

--- setup_database.py
+++ setup_database.py
@@ -7,17 +7,34 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
 
-database_name = 'amphora2'
+# ############
+# # AMPHORA2 #
+# ############
+# database_name = 'amphora2'
 
-for marker in glob.iglob('databases/amphora2/*.orig'):
-    print(marker)
-    seqid = 0
-    out = []
+# for marker in glob.iglob('databases/amphora2/*.orig'):
+#     print(marker)
+#     seqid = 0
+#     out = []
 
-    with bz2.BZ2File(marker, 'rU') as g:
-        for record in SeqIO.parse(g, "fasta"):
-            out.append(SeqRecord(record.seq, id='_'.join([database_name, marker[marker.rfind('/')+1:marker.find('.')], str(seqid)]), description=''))
-            seqid += 1
+#     with bz2.BZ2File(marker, 'rU') as g:
+#         for record in SeqIO.parse(g, "fasta"):
+#             out.append(SeqRecord(record.seq, id='_'.join([database_name, marker[marker.rfind('/')+1:marker.find('.')], str(seqid)]), description=''))
+#             seqid += 1
 
-    with open(marker[:marker.find('.')]+'.faa', 'w') as f:
-        SeqIO.write(out, f, "fasta")
+#     with open(marker[:marker.find('.')]+'.faa', 'w') as f:
+#         SeqIO.write(out, f, "fasta")
+
+# #############
+# # C. parvum #
+# #############
+# database_name = 'cparvum'
+# seqs = []
+# seq_counter = 0
+
+# for seq_record in SeqIO.parse('databases/cparvum/CryptoDB-33_CparvumIowaII_AnnotatedTranscripts.fasta', "fasta"):
+#     seqs.append(SeqRecord(seq_record.seq, id='{}_{}_{}'.format(database_name, seq_record.id.replace('_', '-'), seq_counter), description=''))
+#     seq_counter += 1
+
+# with open('databases/cparvum/cparvum.fna', 'w') as f:
+#     SeqIO.write(seqs, f, "fasta")
--- write_config_file.py
+++ write_config_file.py
@@ -7,6 +7,11 @@ import argparse as ap
 import configparser as cp
 
 
+DB_TYPE_CHOICES = ['n', 'a']
+DB_DNA_CHOICES = ['makeblastdb']
+DB_AA_CHOICES = ['usearch', 'diamond']
+MAP_DNA_CHOICES = ['blastn', 'tblastn', 'diamond']
+MAP_AA_CHOICES = ['usearch', 'diamond']
 MSA_CHOICES = ['muscle', 'mafft', 'opal', 'upp']
 TRIM_CHOICES = ['trimal']
 GENE_TREE1_CHOICES = ['fasttree', 'raxml']
@@ -41,10 +46,14 @@ def read_params():
     p = ap.ArgumentParser(description="")
 
     p.add_argument('-o', '--output', type=str, required=True, help="Specify the output file where to write the configurations. If the file already exists the program won't overwrite the alredy existing file")
+    p.add_argument('-d', '--db_type', required=True, choices=DB_TYPE_CHOICES, help="Specify the type of the database, where 'n' stands for nucleotides and 'a' for amino acids")
 
-    p.add_argument('--map_dna', action='store_true', default=False, help="Specify to add the map_dna section. Note, at least one of map_dna and map_aa must be specified")
-    p.add_argument('--map_aa', action='store_true', default=False, help="Specify to add the map_aa section. Note, at least one of map_dna and map_aa must be specified")
+    p_db = p.add_mutually_exclusive_group(required=True)
+    p_db.add_argument('--db_dna', default=None, choices=DB_DNA_CHOICES, help="")
+    p_db.add_argument('--db_aa', default=None, choices=DB_AA_CHOICES, help="")
 
+    p.add_argument('--map_dna', default=None, choices=MAP_DNA_CHOICES, help="")
+    p.add_argument('--map_aa', default=None, choices=MAP_AA_CHOICES, help="")
     p.add_argument('--msa', required=True, default=None, choices=MSA_CHOICES, help="")
     p.add_argument('--trim', default=None, choices=TRIM_CHOICES, help="Specify to add the trim section")
     p.add_argument('--gene_tree1', default=None, choices=GENE_TREE1_CHOICES, help="Specify to add the gene_tree1 section")
@@ -82,34 +91,90 @@ if __name__ == '__main__':
     args = read_params()
     check_params(args)
     config = cp.ConfigParser()
+    progs = {}
 
-    progs = {
-        'db_aa': {'program_name': 'usearch9.2.64_i86linux32',
-                  'params': '-quiet',
-                  'input': '-makeudb_ublast',
-                  'output': '-output',
-                  'version': '-version',
-                  'command_line': '#program_name# #params# #input# #output#'},
-    }
+    # setting the program for building the nucleotides DB
+    if args.db_dna:
+        if 'makeblastdb' in args.db_dna:
+            db_dna = {'program_name': 'makeblastdb',
+                     'params': '-parse_seqids -dbtype nucl',
+                     'input': '-in',
+                     'output': '-out',
+                     'version': '-version',
+                     'command_line': '#program_name# #params# #input# #output#'}
 
+        progs['db_dna'] = db_dna
+
+    # setting the program for building the amino acids DB
+    if args.db_aa:
+        if 'usearch' in args.db_aa:
+            db_aa = {'program_name': 'usearch9.2.64_i86linux32',
+                     'params': '-quiet',
+                     'input': '-makeudb_ublast',
+                     'output': '-output',
+                     'version': '-version',
+                     'command_line': '#program_name# #params# #input# #output#'}
+        elif 'diamond' in args.db_aa:
+            db_aa = {'program_name': 'diamond',
+                     'params': 'makedb',
+                     'threads': '--threads',
+                     'input': '--in',
+                     'output': '--db',
+                     'version': 'version',
+                     'command_line': '#program_name# #params# #threads# #input# #output#'}
+
+        progs['db_aa'] = db_aa
+
+    # setting the software for mapping the genomes (dna)
     if args.map_dna:
-        progs['map_dna'] = {'program_name': 'tblastn',
-                            'params': '-outfmt "6 saccver qaccver pident length mismatch gapopen sstart send qstart qend evalue bitscore" -evalue 1e-50',
-                            'input': '-subject',
-                            'database': '-query',
-                            'output': '-out',
-                            'version': '-version',
-                            'command_line': '#program_name# #params# #input# #database# #output#'}
+        if 'blastn' in args.map_dna:
+            map_dna = {'program_name': 'blastn',
+                       'params': '-outfmt 6',
+                       'input': '-query',
+                       'database': '-db',
+                       'output': '-out',
+                       'version': '-version',
+                       'command_line': '#program_name# #params# #input# #database# #output#'}
+        elif 'tblastn' in args.map_dna:
+            map_dna = {'program_name': 'tblastn',
+                       'params': '-outfmt "6 saccver qaccver pident length mismatch gapopen sstart send qstart qend evalue bitscore" -evalue 1e-50',
+                       'input': '-subject',
+                       'database': '-query',
+                       'output': '-out',
+                       'version': '-version',
+                       'command_line': '#program_name# #params# #input# #database# #output#'}
+        elif 'diamond' in args.map_dna:
+            map_dna = {'program_name': 'diamond',
+                       'params': 'blastx --quiet --threads 1 --outfmt 6 --more-sensitive --id 50 --max-hsps 35 --top 90',
+                       'input': '--query',
+                       'database': '--db',
+                       'output': '--out',
+                       'version': 'version',
+                       'command_line': '#program_name# #params# #input# #database# #output#'}
 
+        progs['map_dna'] = map_dna
+
+    # setting the software for mapping the proteomes (aa)
     if args.map_aa:
-        progs['map_aa'] = {'program_name': 'usearch9.2.64_i86linux32',
-                           'params': '-quiet -evalue 1e-10 -maxaccepts 8 -maxrejects 32',
-                           'threads': '-threads',
-                           'input': '-ublast',
-                           'database': '-db',
-                           'output': '-blast6out',
-                           'version': '-version',
-                           'command_line': '#program_name# #params# #threads# #input# #database# #output#'}
+        if 'usearch' in args.map_aa:
+            map_aa = {'program_name': 'usearch9.2.64_i86linux32',
+                      'params': '-quiet -evalue 1e-10 -maxaccepts 8 -maxrejects 32',
+                      'threads': '-threads',
+                      'input': '-ublast',
+                      'database': '-db',
+                      'output': '-blast6out',
+                      'version': '-version',
+                      'command_line': '#program_name# #params# #threads# #input# #database# #output#'}
+        elif 'diamond' in args.map_aa:
+            map_aa = {'program_name': 'diamond',
+                      'params': 'blastp --quiet --threads 1 --outfmt 6 --more-sensitive --id 50',
+                      'input': '--query',
+                      'database': '--db',
+                      'output': '--out',
+                      'version': 'version',
+                      'command_line': '#program_name# #params# #input# #database# #output#'}
+
+        progs['map_aa'] = map_aa
 
     # setting the MSA software
     if 'muscle' in args.msa:
@@ -121,7 +186,7 @@ if __name__ == '__main__':
                'command_line': '#program_name# #params# #input# #output#'}
     elif 'mafft' in args.msa:
         msa = {'program_name': 'mafft',
-               'params': '--quiet --auto',
+               'params': '--quiet --anysymbol --auto',
                'version': '--version',
                'command_line': '#program_name# #params# #input# > #output#'}
     elif 'opal' in args.msa:
@@ -130,14 +195,22 @@ if __name__ == '__main__':
                'output': '--out',
                'params': '--quiet',
                'command_line': '#program_name# #params# #input# #output#'}
+
+        if args.db_type == 'a':
+            gene_tree1['params'] += ' --protein'
     elif 'upp' in args.msa:
         msa = {'program_name': 'run-upp.sh',
-               'params': '-m amino -x 1 -M -1 -T 0.66 -B 999999999',
+               'params': '-x 1 -M -1 -T 0.66 -B 999999999',
                'input': '-s',
                'output': '-o',
                'output_path': '-d',
                'version': '--version',
                'command_line': '#program_name# #params# #input# #output_path# #output#'}
+
+        if args.db_type == 'n':
+            gene_tree1['params'] += ' -m dna',
+        elif args.db_type == 'a':
+            gene_tree1['model'] += ' -m amino'
 
     progs['msa'] = msa
 
@@ -148,9 +221,8 @@ if __name__ == '__main__':
                     'params': '-gappyout',
                     'input': '-in',
                     'output': '-out',
-                    'output_path': '-d',
                     'version': '--version',
-                    'command_line': '#program_name# #params# #output_path# #input# #output#'}
+                    'command_line': '#program_name# #params# #input# #output#'}
 
         progs['trim'] = trim
 
@@ -158,18 +230,26 @@ if __name__ == '__main__':
     if args.gene_tree1:
         if 'fasttree' in args.gene_tree1:
             gene_tree1 = {'program_name': 'FastTree-2.1.9-SSE3',
-                          'params': '-quiet -lg -mlacc 2 -slownni -spr 4 -fastest -mlnni 4 -no2nd',
+                          'params': '-quiet -mlacc 2 -slownni -spr 4 -fastest -mlnni 4 -no2nd',
                           'output': '-out',
                           'command_line': '#program_name# #params# #output# #input#'}
+
+            if args.db_type == 'n':
+                gene_tree1['params'] += ' -gtr -nt'
         elif 'raxml' in args.gene_tree1:
             gene_tree1 = {'program_name': 'raxmlHPC',
                           'params': '-p 1989',
-                          'model': '-m',
                           'input': '-s',
                           'output_path': '-w',
                           'output': '-n',
-                          'version': '-v',
-                          'command_line': '#program_name# #model# #params# #output_path# #input# #output#'}
+                          'version': '-v'}
+
+            if args.db_type == 'n':
+                gene_tree1['params'] += ' -m GTRCAT',
+                gene_tree1['command_line'] = '#program_name# #model# #params# #output_path# #input# #output#'
+            elif args.db_type == 'a':
+                gene_tree1['model'] = '-m'
+                gene_tree1['command_line'] = '#program_name# #model# #params# #output_path# #input# #output#'
 
         progs['gene_tree1'] = gene_tree1
 
@@ -178,55 +258,68 @@ if __name__ == '__main__':
         if 'raxml' in args.gene_tree2:
             gene_tree2 = {'program_name': 'raxmlHPC',
                           'params': '-p 1989',
-                          'model': '-m',
                           'database': '-t', # starting tree
                           'input': '-s',
                           'output_path': '-w',
                           'output': '-n',
-                          'version': '-v',
-                          'command_line': '#program_name# #model# #params# #database# #output_path# #input# #output#'}
+                          'version': '-v'}
+
+            if args.db_type == 'n':
+                gene_tree2['params'] += '-p 1989 -m GTRCAT'
+                gene_tree2['command_line'] = '#program_name# #params# #database# #output_path# #input# #output#'
+            elif args.db_type == 'a':
+                gene_tree2['model'] = '-m',
+                gene_tree2['command_line'] = '#program_name# #model# #params# #database# #output_path# #input# #output#'
 
         progs['gene_tree2'] = gene_tree2
 
     # setting tree1
-    if args.tree1:
-        if 'astral' in args.tree1:
-            tree1 = {'program_name': 'java -jar /CM/tools/astral-4.11.1/astral.4.11.1.jar',
-                     'input': '-i',
-                     'output': '-o',
-                     # 'version': '--help',
-                     'version': '-i /CM/tools/astral-4.11.1/test_data/song_mammals.424.gene.tre',
-                     'command_line': '#program_name# #input# #output#'}
-        if 'astrid' in args.tree1:
-            tree1 = {'program_name': 'ASTRID',
-                     'input': '-i',
-                     'params': '-m auto',
-                     'output': '-o',
-                     'version': '--help',
-                     'command_line': '#program_name# #input# #params# #output#'}
-        elif 'fasttree' in args.tree1:
-            tree1 = {'program_name': 'FastTreeMP-2.1.9-SSE3',
-                     'params': '-quiet -lg -mlacc 2 -slownni -spr 4 -fastest -mlnni 4 -no2nd',
-                     'output': '-out',
-                     'environment': 'OMP_NUM_THREADS=3',
-                     'command_line': '#program_name# #params# #output# #input#'}
-        elif 'raxml' in args.tree1:
-            tree1 = {'program_name': 'raxmlHPC-PTHREADS-SSE3',
-                     'params': '-m PROTCATLG -p 1989',
-                     'threads': '-T',
-                     'input': '-s',
-                     'output_path': '-w',
-                     'output': '-n',
-                     'version': '-v',
-                     'command_line': '#program_name# #params# #threads# #database# #output_path# #input# #output#'}
+    if 'astral' in args.tree1:
+        tree1 = {'program_name': 'java -jar /CM/tools/astral-4.11.1/astral.4.11.1.jar',
+                 'input': '-i',
+                 'output': '-o',
+                 # 'version': '--help',
+                 'version': '-i /CM/tools/astral-4.11.1/test_data/song_mammals.424.gene.tre',
+                 'command_line': '#program_name# #input# #output#'}
+    if 'astrid' in args.tree1:
+        tree1 = {'program_name': 'ASTRID',
+                 'input': '-i',
+                 'params': '-m auto',
+                 'output': '-o',
+                 'version': '--help',
+                 'command_line': '#program_name# #input# #params# #output#'}
+    elif 'fasttree' in args.tree1:
+        tree1 = {'program_name': 'FastTreeMP-2.1.9-SSE3',
+                 'params': '-quiet -mlacc 2 -slownni -spr 4 -fastest -mlnni 4 -no2nd',
+                 'output': '-out',
+                 'environment': 'OMP_NUM_THREADS=3',
+                 'command_line': '#program_name# #params# #output# #input#'}
 
-        progs['tree1'] = tree1
+        if args.db_type == 'n':
+            tree1['params'] += ' -gtr -nt'
+
+    elif 'raxml' in args.tree1:
+        tree1 = {'program_name': 'raxmlHPC-PTHREADS-SSE3',
+                 'params': '-p 1989',
+                 'threads': '-T',
+                 'input': '-s',
+                 'output_path': '-w',
+                 'output': '-n',
+                 'version': '-v',
+                 'command_line': '#program_name# #params# #threads# #database# #output_path# #input# #output#'}
+
+        if args.db_type == 'n':
+            tree1['params'] += ' -m GTRCAT'
+        elif args.db_type == 'a':
+            tree1['params'] += ' -m PROTCATLG'
+
+    progs['tree1'] = tree1
 
     # setting tree2
     if args.tree2:
         if 'raxml' in args.tree2:
             tree2 = {'program_name': 'raxmlHPC-PTHREADS-SSE3',
-                     'params': '-m PROTCATLG -p 1989',
+                     'params': '-p 1989',
                      'threads': '-T',
                      'database': '-t', # starting tree
                      'input': '-s',
@@ -234,6 +327,11 @@ if __name__ == '__main__':
                      'output': '-n',
                      'version': '-v',
                      'command_line': '#program_name# #params# #threads# #database# #output_path# #input# #output#'}
+
+            if args.db_type == 'n':
+                tree2['params'] += ' -m GTRCAT'
+            elif args.db_type == 'a':
+                tree2['params'] += ' -m PROTCATLG'
 
         progs['tree2'] = tree2
 
