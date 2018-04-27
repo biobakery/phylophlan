8810ae456685b6c3d7a263e2edb79c645c56bb0c
Francesco Asnicar <5664022+fasnicar@users.noreply.github.com>
2018-04-27 11:44:36 +0200

    start developing the download of reference/core proteomes

--- setup_database.py
+++ setup_database.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 
+__author__ = ('Francesco Asnicar (f.asnicar@unitn.it), '
+              'Francesco Beghini (francesco.beghini@unitn.it), '
+              'Mattia Bolzan (mattia.bolzan@unitn.it), '
+              'Nicola Segata (nicola.segata@unitn.it)')
+__version__ = '0.03'
+__date__ = '27 April 2018'
+
+
 import sys
 import bz2
 import glob
@@ -8,11 +16,15 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
 import argparse as ap
+from urllib.request import urlretrieve
+import time
 
 
 DB_TYPE_CHOICES = ['n', 'a']
 GENOME_EXTENSION = '.fna'
 PROTEOME_EXTENSION = '.faa'
+DOWNLOAD_URL = "https://bitbucket.org/nsegata/phylophlan/downloads/"
+TAXA2CORE_FILE = "taxa2core.tsv.bz2"
 
 
 def info(s, init_new_line=False, exit=False, exit_value=0):
@@ -40,21 +52,24 @@ def error(s, init_new_line=False, exit=False, exit_value=1):
 def read_params():
     p = ap.ArgumentParser(formatter_class=ap.ArgumentDefaultsHelpFormatter)
 
-    p.add_argument('-i', '--input', required=True, type=str,
-                   help=("Specify the path to the folder containing the markers file or "
-                         "file (multi-fasta) of markers"))
-    p.add_argument('-o', '--output', required=False, type=str, default=None,
-                   help=("Specify path to the output folder where to save the database, "
-                         "if not specified the input path (specified via -i, --input) "
-                         "will be used"))
-    p.add_argument('-d', '--db_name', required=True, type=str,
-                   help="Specify the name of the output database")
-    p.add_argument('-e', '--input_extension', type=str, required=False, default=None,
-                   help=("Specify the extension of the input file(s) specified via -i, "
-                         "--input"))
-    p.add_argument('-t', '--db_type', required=False, default=None, choices=DB_TYPE_CHOICES,
-                   help=("Specify the type of the database, where 'n' stands for "
-                         "nucleotides and 'a' for amino acids"))
+    group = p.add_mutually_exclusive_group(required=True)
+    group.add_argument('-i', '--input', type=str,
+                       help=("Specify the path to either the folder containing the marker "
+                             "files or the file of markers, in (multi-)fasta format"))
+    group.add_argument('-g', '--get_core_proteins', type=str, default=None,
+                       help=('Specify the taxonomic label for which download the set of '
+                             'core proteins. The label can represents: '
+                             'a species ("--get_core_proteins s__Escherichia_coli") or a '
+                             'a genus ("--get_core_proteins g__Escherichia")'))
+
+    p.add_argument('-o', '--output', type=str, required=True, default=None,
+                   help="Specify path to the output folder where to save the database")
+    p.add_argument('-d', '--db_name', type=str, help="Specify the name of the output database")
+    p.add_argument('-e', '--input_extension', type=str, default=None,
+                   help="Specify the extension of the input file(s) specified via -i/--input")
+    p.add_argument('-t', '--db_type', default=None, choices=DB_TYPE_CHOICES,
+                   help=('Specify the type of the database, where "n" stands for '
+                         'nucleotides and "a" for amino acids'))
     p.add_argument('-x', '--output_extension', type=str, default=None,
                    help="Set the database output extension")
     p.add_argument('--overwrite', action='store_true', default=False,
@@ -65,24 +80,32 @@ def read_params():
 
 
 def check_params(args, verbose=False):
-    if (not os.path.isdir(args.input)) and (not os.path.isfile(args.input)):
-        error('input must be either a folder or a file', exit=True)
+    if not os.path.isdir(args.output):
+        error('output param is not a directory', exit=True)
 
-    if not args.output:
-        args.output = os.path.dirname(args.input) if os.path.isfile(args.input) else args.input
+    if args.input:
+        if (not os.path.isdir(args.input)) and (not os.path.isfile(args.input)):
+            error('input must be either a folder or a file', exit=True)
 
-        if verbose:
-            info('Setting output to "{}"\n'.format(args.output))
+        if not args.db_name:
+            error('-d/--db_name must be specified', exit=True)
 
-    if os.path.isdir(args.input):
-        if not args.input_extension:
-            error('input is a folder, hence --input_extension must be specified',
-                  exit=True)
+        if os.path.isdir(args.input):
+            if not args.input_extension:
+                error('input is a folder, hence --input_extension must be specified', exit=True)
+
+    if args.get_core_proteins:
+        args.input = args.output
+
+        if not args.db_name:
+            args.db_name = args.get_core_proteins
+
+            if verbose:
+                info('Setting db_name to "{}"\n'.format(args.db_name))
 
     if not args.db_type:
         if not args.output_extension:
-            error('either -t (--db_type) or -x (--output_extension) must be specified',
-                  exit=True)
+            error('either -t (--db_type) or -x (--output_extension) must be specified', exit=True)
         else:
             if not args.output_extension.startswith('.'):
                 args.output_extension = '.' + args.output_extension
@@ -103,21 +126,106 @@ def check_params(args, verbose=False):
                   "know which one to pick!", exit=True)
 
 
+def byte_to_megabyte(byte):
+    """
+    Convert byte value to megabyte
+    """
+
+    return (byte / 1048576)
+
+
+class ReportHook():
+
+    def __init__(self):
+        self.start_time = time.time()
+
+    def report(self, blocknum, block_size, total_size):
+        """
+        Print download progress message
+        """
+
+        if blocknum == 0:
+            self.start_time = time.time()
+
+            if total_size > 0:
+                info("Downloading file of size: {:.2f} MB\n".format(byte_to_megabyte(total_size)))
+        else:
+            total_downloaded = blocknum * block_size
+            status = "{:3.2f} MB ".format(byte_to_megabyte(total_downloaded))
+
+            if total_size > 0:
+                percent_downloaded = total_downloaded * 100.0 / total_size
+                # use carriage return plus sys.stderr to overwrite stderr
+                download_rate = total_downloaded / (time.time() - self.start_time)
+                estimated_time = (total_size - total_downloaded) / download_rate
+                estimated_minutes = int(estimated_time / 60.0)
+                estimated_seconds = estimated_time - estimated_minutes * 60.0
+                status += ("{:3.2f} %  {:5.2f} MB/sec {:2.0f} min {:2.0f} sec "
+                           .format(percent_downloaded, byte_to_megabyte(download_rate),
+                                   estimated_minutes, estimated_seconds))
+
+            status += "        \r"
+            info(status)
+
+
+def download(url, download_file, verbose=False):
+    """
+    Download a file from a url
+    """
+
+    if not os.path.isfile(download_file):
+        try:
+            if verbose:
+                info('Downloading "{}"\n'.format(url))
+
+            urlretrieve(url, download_file, reporthook=ReportHook().report)
+        except EnvironmentError:
+            error('unable to download "{}"'.format(url))
+    elif verbose:
+        info('File "{}" already present!\n'.format(download_file))
+
+
 def create_folder(output, verbose=False):
-    if not os.path.isdir(output):
+    if not os.path.exists(output):
         if verbose:
             info('Creating output database folder "{}"\n'.format(output))
 
-        os.mkdir(output)
+        os.mkdir(output, mode=0o775)
     elif verbose:
         info("Output database folder already present\n")
+
+
+def get_core_proteins(taxa2core_file, download_url, taxa_label, verbose=False):
+    download(download_url, taxa2core_file, verbose=verbose)
+    core_proteins = {}
+    metadata = None
+
+    for r in bz2.open(taxa2core_file, 'rt'):
+        if r.startswith('#'):
+            metadata = r.strip()
+        elif r.strip().split('\t')[1].endswith(taxa_label):
+            core_proteins[r.strip().split('\t')[1]] = r.strip().split('\t')[-1].split(';')
+        elif taxa_label in r.strip().split('\t')[1]:
+            core_proteins[r.strip().split('\t')[1]] = None
+
+    if not len(core_proteins):
+        error('no entry found for "{}", please check the taxonomic label provided'
+              .format(taxa_label), exit=True)
+    elif len(core_proteins) > 1:
+        error('{} entries found for "{}":\n{}    please check the taxonomic label provided'
+              .format(len(core_proteins), taxa_label, '    - {}\n'.join(core_proteins.keys())),
+              exit=True)
+
+    for prot_url in core_proteins.values():
+        prot_file = os.path.basename(prot_url)  # DA VERIFICARE!!!
+        download(prot_url, prot_file, verbose=verbose)
 
 
 def create_database(db_name, inputt, input_ext, output, overwrite, verbose=False):
     seqs = []
 
     if os.path.exists(output) and (not overwrite):
-        error('output file exists --overwrite not provided', exit=True)
+        error('output file exists and --overwrite not specified', exit=True)
 
     if os.path.isdir(inputt):
         for marker in glob.iglob(os.path.join(inputt, '*' + input_ext + '*')):
@@ -151,6 +259,11 @@ if __name__ == '__main__':
     args = read_params()
     check_params(args, verbose=args.verbose)
     create_folder(os.path.join(args.output), verbose=args.verbose)
+
+    if args.get_core_proteins:
+        get_core_proteins(TAXA2CORE_FILE, os.path.join(TAXA2CORE_FILE, DOWNLOAD_URL),
+                          args.get_core_proteins, verbose=args.verbose)
+
     create_database(args.db_name, args.input, args.input_extension,
                     os.path.join(args.output, args.db_name + args.output_extension),
                     args.overwrite, verbose=args.verbose)
