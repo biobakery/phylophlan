85d3326dcbcbb10a231bf8f5b78e9e8951617f07
Francesco Asnicar <francesco.asnicar@gmail.com>
2019-06-12 12:41:45 +0000

    retrieve_coli_bins.sh edited online with Bitbucket

--- examples/04_coli/retrieve_coli_bins.sh
+++ examples/04_coli/retrieve_coli_bins.sh
@@ -1,7 +1,8 @@
-#usr/bin/bash!
+#!/bin/bash
+
 
 mkdir -p examples/04_coli/input_bins
 
-for i in $(cat examples/03_metagenomic/output_metagenomic.tsv | grep Escherichia_coli | cut -f1); do
-cp -a examples/03_metagenomic/input_metagenomic/$i.fna  examples/04_coli/input_bins
+for i in $(grep kSGB_10068 examples/03_metagenomic/output_metagenomic.tsv | cut -f1); do
+    cp -a examples/03_metagenomic/input_metagenomic/$i.fna examples/04_coli/input_bins/;
 done;
