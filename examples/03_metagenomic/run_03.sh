4de317544d16a41db8be091f00bb88bb18d0c141
Claudia Mengoni <43252093+Cengoni@users.noreply.github.com>
2019-06-18 15:46:46 +0200

    fixes in output handling in draw_metagenomic and strain_finder

--- examples/03_metagenomic/run_03.sh
+++ examples/03_metagenomic/run_03.sh
@@ -6,6 +6,6 @@ phylophlan_metagenomic.py -i examples/03_metagenomic/input_metagenomic \
 2>&1 | tee examples/03_metagenomic/logs/phylophlan_metagenomic.log
 
 #Draw heatmaps
-phylophlan_draw_heatmaps.py -i examples/03_metagenomic/output_metagenomic.tsv \
- --map examples/03_metagenomic/bin2meta.tsv --top 20 --verbose \
-2>&1 | tee examples/03_metagenomic/logs/phylophlan_draw_heatmaps.log
+phylophlan_draw_metagenomic.py -i examples/03_metagenomic/output_metagenomic.tsv \
+ --map examples/03_metagenomic/bin2meta.tsv -o examples/03_metagenomic/output_heatmap \
+--top 20 --verbose 2>&1 | tee examples/03_metagenomic/logs/phylophlan_draw_metagenomic.log
