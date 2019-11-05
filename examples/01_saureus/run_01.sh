c1aac4604543976ae0c63d7ad0ad0e0202f5e5f8
Francesco Asnicar <5664022+fasnicar@users.noreply.github.com>
2019-11-05 17:03:33 +0100

    updating example01 commands

--- examples/01_saureus/run_01.sh
+++ examples/01_saureus/run_01.sh
@@ -56,4 +56,14 @@ phylophlan2.py \
 
 # Visualize the phylogenetic tree with GraPhlAn
 # GraPhlAn is Python2-based and have different requirements than PhyloPhlAn2
-graphlan/run_graphlan_isolates.sh
+echo "GraPhlAn annotate"
+graphlan_annotate.py \
+    --annot graphlan/isolates_annotation.txt \
+    output_isolates/RAxML_bestTree.input_isolates_refined.tre \
+    graphlan/isolates_annotated.xml
+
+echo "GraPhlAn draw"
+graphlan.py \
+    graphlan/isolates_annotated.xml \
+    graphlan/saureus_isolates.png \
+    --dpi 300
